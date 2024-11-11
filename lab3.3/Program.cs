8fb24e09acd2c374d0a7dfdf5bd3f280c4d7b25f
пк <@DESKTOP-81SKH73>
2024-11-11 23:20:39 +0200

    Добавьте файлы проекта.

---
+++ lab3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите  n: ");
+    int n = int.Parse(Console.ReadLine());
+    double s = 0;
+    for (int i = 0; i <= n; i += 2)
+    {
+        i += 2;
+        int factorial = 1;
+            factorial *= i;
+        double term = Math.Pow((2 * x), i) / factorial;
+        if ((i / 2) % 2 == 0)
+        {
+            s += term;
+        }
+        else
+        {
+            s -= term;
+        }
+    }
+    Console.WriteLine($"Сумма ряда: {s}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
